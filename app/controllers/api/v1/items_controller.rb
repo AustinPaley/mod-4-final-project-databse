02e83223accbe3a6640854eb61fdf7a525e40b0a
AustinPaley <austinpaley32@gmail.com>
2018-06-20 15:11:59 -0400

    Show Added

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,6 +10,11 @@ class Api::V1::ItemsController < ApplicationController
     render json: @item
   end
 
+  def show
+    @item = Item.find(params[:id])
+    render json: @item
+  end
+
   def destroy
     Item.destroy(params[:id])
   end
