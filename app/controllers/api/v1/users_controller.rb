dffd1b77228438384d8d0a836bd950c66fb902f7
AustinPaley <austinpaley32@gmail.com>
2018-06-21 17:29:44 -0400

    Stash

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :password)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_06_18_180029) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
